74950773a92f4d9b7299717f1c01926ba7979c61
Alimobasheri <a.mobasheri@icloud.com>
2020-11-16 18:22:13 +0330

    change app layout

--- pages/_app.js
+++ pages/_app.js
@@ -16,11 +16,23 @@ import {makeStyles} from '@material-ui/core/styles'
 import Nav from '../src/components/nav/nav'
 import ResponsiveDrawer from '../src/components/responsive-drawer'
 
+const useStyles = makeStyles(theme => ({
+  appContainer: {
+    overflowX: 'hidden',
+    width: '100vw'
+  },
+  componentWrapper: {
+    padding: `${theme.spacing(1)} 0`,
+    flexGrow: 0
+  }
+}))
+
 function MyApp({ Component, pageProps }) {
   const [store, setStore] = useState(storeFactory())
   const [openMobileDrawer, setOpenMobileDrawer] = useState(false)
   const router = useRouter()
   const bodyContainer = useRef()
+  const classes = useStyles()
   useEffect(() => {
     if(typeof window !== 'undefined') {
       setStore(storeFactory())
@@ -36,25 +48,26 @@ function MyApp({ Component, pageProps }) {
       <Box
       ref={bodyContainer} paddingTop="0">
         <Grid
-        container>
+        container
+        justify="space-evenly"
+        alignItems="center">
           <Grid
           item
           xs={0}
           md={2}>
-            <Box
-            maxWidth="100%">
             <ResponsiveDrawer
             toggled={openMobileDrawer}
             toggleDrawer={setOpenMobileDrawer} />
-            </Box>
           </Grid>
           <Grid
           item
           xs={12}
           md={10}
-          spacing={3}>
+          spacing={1}>
             <Grid
-            container>
+            container
+            justify="space-between"
+            alignItems="center">
               <Grid
               item
               xs={12}>
@@ -65,6 +78,7 @@ function MyApp({ Component, pageProps }) {
               </Grid>
               <Grid
               item
+              className={classes.componentWrapper}
               xs={12}>
                 <Component {...pageProps} />
               </Grid>
