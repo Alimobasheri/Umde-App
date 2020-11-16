71550b303b8cd42572b2a5b99dc3f4b89f3c1a00
Alimobasheri <a.mobasheri@icloud.com>
2020-11-16 13:15:58 +0330

    remove example header

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,15 @@ import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 
+import Nav from '../src/components/nav/nav'
+
 import Items from "../src/containers/items/items";
 import NewItemForm from '../src/containers/new-item-form/new-item-form';
 
 export default function App() {
   return (
-    <Box p={2}>
-      <Grid container xs={12} spacing={1}>
-        <Grid item xs={12}>
-          <h1>به اپلیکیشن عامده خوش آمدید.</h1>
-        </Grid>
+    <Box p={2} marginY={3}>
+      <Grid container spacing={1}>
         <Grid item xs={12} md={9}>
           <Items />
         </Grid>
