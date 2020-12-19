7d24f95f5e8aa1ef2208174bc2401f86cfa8526e
Alimobasheri <a.mobasheri@icloud.com>
2020-12-19 11:46:47 +0330

    Fix: import url typo

--- src/components/Items-Components/new-item-form/new-item-form.js
+++ src/components/Items-Components/new-item-form/new-item-form.js
@@ -3,7 +3,7 @@ import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import {makeStyles} from '@material-ui/core/styles'
-import Typography from '@material-ui/core/typography'
+import Typography from '@material-ui/core/Typography'
 import { Box } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
