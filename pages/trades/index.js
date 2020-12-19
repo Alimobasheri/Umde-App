b2c6bf9687df8f16e9e9b1fe4c83df06bd8a74d4
Alimobasheri <a.mobasheri@icloud.com>
2020-12-19 11:34:04 +0330

    Create: trades page

---
+++ pages/trades/index.js
@@ -0,0 +1,10 @@
+import { Box } from "@material-ui/core";
+import NewFactorForm from "../../src/containers/new-factor-form";
+
+export default function Trades() {
+    return (
+        <Box>
+            <NewFactorForm />
+        </Box>
+    )
+}
