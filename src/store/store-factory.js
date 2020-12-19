c8c656d47338cdda6087e3d175d3222d8eee2d41
Alimobasheri <a.mobasheri@icloud.com>
2020-12-19 11:16:26 +0330

    add factors reducer

--- src/store/store-factory.js
+++ src/store/store-factory.js
@@ -1,12 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 
 import items from '../reducers/items'
+import factors from '../reducers/factors'
 
 const saver = store => next => action => {
-    console.log(store.getState())
     const result = next(action)
     localStorage['Umde-App-Store'] = JSON.stringify(store.getState())
-    console.log(JSON.parse(localStorage['Umde-App-Store']))
     return result
 }
 
@@ -18,15 +17,33 @@ const INITIAL_STATE = {
         viewingItem: {},
         editingItem: {},
         addedItemsCount: 0,
+        lastRegisteredId: 10000
+    },
+    factors: {
+        savedFactors: [],
+        newFactor: {
+            costumer: "",
+            date: "",
+            mainPaymentMethod: "",
+            factorItems: [],
+            totalItemsCount: 0,
+            totalItemsPrice: 0,
+            payments: [],
+            pastAccounts: [],
+            paid: false
+        },
+        editingFactor: {},
+        savedFactorsCount: 0,
+        lastRegisteredFactorId: 100000,
     }
 }
 
 export default function storeFactory(initialState=INITIAL_STATE) {
     if (typeof window === 'undefined') {
-        return createStore(combineReducers({items}), INITIAL_STATE)
+        return createStore(combineReducers({items, factors}), INITIAL_STATE)
     }
     return applyMiddleware(saver)(createStore)(
-        combineReducers({items}),
+        combineReducers({items, factors}),
         typeof window !== 'undefined' && localStorage['Umde-App-Store'] ? 
             JSON.parse(localStorage['Umde-App-Store']) :
             initialState
